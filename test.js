e6b1b51dcab75da16b20c21b3c7fcb66b1958f57
Federico Ogrizovic <fogrizovic@gmail.com>
2024-07-29 11:42:32 -0300

    Revert "make load test data at startup"
    This reverts commit 93f44e36fd3a24c9e014959b1c10328f2c68f8e2.

--- test.js
+++ test.js
@@ -19,24 +19,22 @@ function loadTestData() {
       url: "https://www.youtube.com/embed/kKu5bGBMeis?si=RpCBOp40LR8HBIDh",
     },
     {
-      id: 4,
-      title: "clase 4",
-      isCompleted: false,
-      url: "https://www.youtube.com/embed/wEJBpskTXHw?si=xIpCB_1ZpfKBGJy7",
-    },
-    {
-      id: 5,
-      title: "clase 5",
-      isCompleted: false,
-      url: "https://www.youtube.com/embed/3XZtl07nm8g?si=4d4tbRzNo35AVXeg",
-    },
-    {
-      id: 6,
-      title: "clase 6",
-      isCompleted: false,
-      url: "https://www.youtube.com/embed/TJmrJJqSi0s?si=sCP2kl4Kg1EnPI7e",
-    },
+        id: 4,
+        title: "clase 4",
+        isCompleted: false,
+        url: "https://www.youtube.com/embed/wEJBpskTXHw?si=xIpCB_1ZpfKBGJy7",
+      },
+      {
+        id: 5,
+        title: "clase 5",
+        isCompleted: false,
+        url: "https://www.youtube.com/embed/3XZtl07nm8g?si=4d4tbRzNo35AVXeg",
+      },
+      {
+        id: 6,
+        title: "clase 6",
+        isCompleted: false,
+        url: "https://www.youtube.com/embed/TJmrJJqSi0s?si=sCP2kl4Kg1EnPI7e",
+      },
   ]);
 }
-
-loadTestData();
