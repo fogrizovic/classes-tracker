34f77dccf94db2bb5250e3d67acc7867fd033dce
Federico Ogrizovic <fogrizovic@gmail.com>
2024-07-29 17:41:21 -0300

    add form for adding classes

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const CLASSES_LIST_KEY = "course_classes_list";
 
 function getClassesList() {
   let result = localStorage.getItem(CLASSES_LIST_KEY);
-  if (result != null) {
-    result = JSON.parse(result);
-  } else {
-    result = [];
-  }
-
-  return result;
+  return result ? JSON.parse(result) : [];
 }
 
 function storeClassesList(classesList) {
@@ -16,24 +10,50 @@ function storeClassesList(classesList) {
   localStorage.setItem(CLASSES_LIST_KEY, classesString);
 }
 
-// populate #classes-list element
-const classesListEl = document.getElementById("classes-list");
+function displayClassesList() {
+  const classesListEl = document.getElementById("classes-list");
+  classesListEl.innerHTML = "";
 
-const classesList = getClassesList();
+  const classesList = getClassesList();
 
-classesList.forEach((classItem) => {
-  classesListEl.insertAdjacentHTML(
-    "beforeend",
-    `
-        <div id="${classItem.id}" class="classesList__item">
-            <div class="item-title">Title: ${classItem.title}</div>
-            <div class="item-isCompleted">Completed: ${classItem.isCompleted}</div>
-            <iframe class="item-video" src="${classItem.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; " referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <div class="item-actions">
-                <button>Edit</button>
-                <button>Delete</button>
-            </div> 
-        </div>
-        `
-  );
+  classesList.forEach((classItem) => {
+    classesListEl.insertAdjacentHTML(
+      "beforeend",
+      `
+            <div id="${classItem.id}" class="classesList__item">
+                <div class="item-title">Title: ${classItem.title}</div>
+                <div class="item-isCompleted">Completed: ${classItem.isCompleted}</div>
+                <iframe class="item-video" src="${classItem.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; " referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <div class="item-actions">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div> 
+            </div>
+            `
+    );
+  });
+}
+
+var form = document.getElementById("addClassForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let classesList = getClassesList();
+
+  let obj = {
+    id: classesList.length + 1,
+    title: document.getElementById("title").value,
+    isCompleted: false,
+    url: document.getElementById("url").value,
+  };
+
+  classesList.push(obj);
+
+  storeClassesList(classesList);
+
+  displayClassesList();
 });
+
+// 1st time display classes
+displayClassesList();
